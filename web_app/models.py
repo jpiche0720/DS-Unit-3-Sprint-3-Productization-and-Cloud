3c1809073386b6e31d30afc9ea2a8923c1095efa
joshua piche <jpiche0720@gmail.com>
2020-06-12 19:21:58 -0400

    Assignment 2

--- web_app/models.py
+++ web_app/models.py
@@ -5,13 +5,26 @@ db = SQLAlchemy()
 
 migrate = Migrate()
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128))
-    author_id = db.Column(db.String(128))
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    screen_name = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    followers_count = db.Column(db.Integer)
+    #lateset_tweet_id = db.Column(db.BigInteger)
 
-    def __repr__(self):
-        return f"<Book {self.id} {self.title}>"
+
+class Tweet(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
+    full_text = db.Column(db.String(500))
+    embedding = db.Column(db.PickleType)
+
+    user = db.relationship('User', backref=db.backref('tweets', lazy=True))
+
+
+
+
     
 def parse_records(database_records):
     """
--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -7,9 +7,8 @@ load_dotenv()
 BASILICA_API_KEY = os.getenv('BASILICA_API_KEY')
 
 connection = basilica.Connection(BASILICA_API_KEY)
-print(type(connection))
 
 if __name__ == '__main__':
-
+    print(type(connection))
     embeddings = connection.embed_sentences(["Hello world!", "How are you?"])
     print(list(embeddings)) # [[0.8556405305862427, ...], ...]
